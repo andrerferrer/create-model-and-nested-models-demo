0ba99267c246f7524ec5897d5fb3c79f6842c67a
Andre Ferrer <andrerferrer@hotmail.com>
2020-10-04 00:46:16 -0300

    accept nested attrs for tags on restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,14 @@
 class Restaurant < ApplicationRecord
+  
   STRONG_PARAMS = [
     :name,
     :address,
     tag_ids: []
   ]
-
+  
   has_many :reviews, dependent: :destroy
   has_many :restaurant_tags, dependent: :destroy
   has_many :tags, through: :restaurant_tags
+  accepts_nested_attributes_for :tags
 
 end
